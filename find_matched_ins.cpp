ddb989ea6fd7c50b7c484e0b0071687524be64b8
pILLOW-1 <71083469+pILLOW-1@users.noreply.github.com>
2021-09-25 15:49:25 +0800

    Update find_matched_ins.cpp

--- find_matched_ins.cpp
+++ find_matched_ins.cpp
@@ -9,7 +9,7 @@ int find_matched_ins(Matrix INS_Time, double current_time)
 	temp_time.base = NULL;
 	temp_time.row = INS_Time.row;
 	temp_time.col = INS_Time.col;
-	CreateMatrix(temp_time);
+	CreateMatrix(temp_time.base,temp_time.col,temp_time.row);
 	for (int i = 0; i < INS_Time.row; i++)
 	{
 		temp_time.base[i][0] = fabs(INS_Time.base[i][0] - current_time);
@@ -21,7 +21,7 @@ int find_matched_ins(Matrix INS_Time, double current_time)
 	if (index_LF % 2)
 	{
 		Matrix temp_imu_sec;
-		CopyMatrix(INS_Time, temp_imu_sec);
+		CopyMatrix(INS_Time, &temp_imu_sec);
 		temp_imu_sec.base[index_LF][0] = INS_Time.base[0][0];
 		for (int i = 0; i < INS_Time.row; i++)
 		{
@@ -31,8 +31,8 @@ int find_matched_ins(Matrix INS_Time, double current_time)
 		for (int i = 0; i < INS_Time.row; i++)
 			if (temp_time.base[i][0] < temp_time.base[index_LF][0])
 				index_LF = i;
-		DestroyMatrix(temp_imu_sec);
+		DestroyMatrix(temp_imu_sec.base,temp_imu_sec.col,temp_imu_sec.row);
 	}
-	DestroyMatrix(temp_time);
+	DestroyMatrix(temp_time.base,temp_time.col,temp_time.row);
 	return index_LF;
 }
