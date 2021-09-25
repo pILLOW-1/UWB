eca954d2091281f94838f257a9bc4457197953da
pILLOW-1 <71083469+pILLOW-1@users.noreply.github.com>
2021-09-25 15:37:08 +0800

    Update Cn2b.h

--- Cn2b.h
+++ Cn2b.h
@@ -1,5 +1,4 @@
 #pragma once
 #include "config.h"
 
-void Cn2b(double yaw, double pitch, double roll, Matrix& cnb, Matrix& Q);
 void Cn2b(double yaw, double pitch, double roll, double cnb[3][3], double Q[4]);
